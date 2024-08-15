bf3c66fed770a3c44f8a9ee0a3bf412dc889e7f1
Tobias Büchel <tobi.buechel@gmail.com>
2024-08-15 13:36:39 +0200

    Add /time endpoint with middleware data passed to controller

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,29 @@ import express from 'express'
 import { logging, loggingWithOptions } from './logging'
 import { scramble } from './scramble'
 import { toNumbers } from './toNumbers'
+import { requestTime } from './requestTime'
 
 const app = express()
 const port = 3003
 
 app.use(bodyParser.json())
 
-// Middlewares
+// Middleware Functions
+app.use('/', requestTime)
 app.use('/api', logging)
 app.use('/api/2', loggingWithOptions({ showIp: false }))
 
 // Enpoints
+app.get('/time', (req, res) => {
+  res.send({ requestTime: req['requestTime'] })
+})
+
 app.get('/api/1', (req, res) => {
-  res.send('Ok')
+  res.sendStatus(204)
 })
 
 app.get('/api/2', (req, res) => {
-  res.send('Ok')
+  res.sendStatus(204)
 })
 
 app.get('/api/scramble', scramble, (req, res) => {
